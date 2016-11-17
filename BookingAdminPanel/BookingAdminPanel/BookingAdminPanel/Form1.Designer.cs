9d7f1668adf4bb3c03ba175736a0ae005ed0000e
Ilgiz <ilgiz1996@mail.ru>
2016-11-18 00:01:18 +0300

    add desktop admin panel
    admin panel

---
+++ BookingAdminPanel/BookingAdminPanel/BookingAdminPanel/Form1.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace BookingAdminPanel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.administratorOptionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addNewHotelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteAHotelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hotelsViewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.bookingDataSet = new BookingAdminPanel.BookingDataSet();
+            this.hotelsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.hotelsTableAdapter = new BookingAdminPanel.BookingDataSetTableAdapters.HotelsTableAdapter();
+            this.addNumberToHotelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookingDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hotelsBindingSource)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.administratorOptionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(711, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // administratorOptionsToolStripMenuItem
+            // 
+            this.administratorOptionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addNewHotelToolStripMenuItem,
+            this.addNumberToHotelToolStripMenuItem,
+            this.deleteAHotelToolStripMenuItem,
+            this.hotelsViewToolStripMenuItem});
+            this.administratorOptionsToolStripMenuItem.Name = "administratorOptionsToolStripMenuItem";
+            this.administratorOptionsToolStripMenuItem.Size = new System.Drawing.Size(135, 20);
+            this.administratorOptionsToolStripMenuItem.Text = "Administrator options";
+            // 
+            // addNewHotelToolStripMenuItem
+            // 
+            this.addNewHotelToolStripMenuItem.Name = "addNewHotelToolStripMenuItem";
+            this.addNewHotelToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.addNewHotelToolStripMenuItem.Text = "Add new Hotel";
+            this.addNewHotelToolStripMenuItem.Click += new System.EventHandler(this.addNewHotelToolStripMenuItem_Click);
+            // 
+            // deleteAHotelToolStripMenuItem
+            // 
+            this.deleteAHotelToolStripMenuItem.Name = "deleteAHotelToolStripMenuItem";
+            this.deleteAHotelToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.deleteAHotelToolStripMenuItem.Text = "Delete a Hotel";
+            this.deleteAHotelToolStripMenuItem.Click += new System.EventHandler(this.deleteAHotelToolStripMenuItem_Click);
+            // 
+            // hotelsViewToolStripMenuItem
+            // 
+            this.hotelsViewToolStripMenuItem.Name = "hotelsViewToolStripMenuItem";
+            this.hotelsViewToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.hotelsViewToolStripMenuItem.Text = "Hotels View";
+            this.hotelsViewToolStripMenuItem.Click += new System.EventHandler(this.hotelsViewToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(21, 49);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(661, 273);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add new Hotel";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(414, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 62);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 103);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "Category";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(6, 129);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 2;
+            this.textBox4.Text = "Picture Url";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 77);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "Name";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Location";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.textBox5);
+            this.groupBox2.Controls.Add(this.textBox6);
+            this.groupBox2.Location = new System.Drawing.Point(12, 43);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(661, 273);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Delete Hotel";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(562, 267);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(414, 151);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 62);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(6, 51);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 3;
+            this.textBox5.Text = "Hotel Location";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(6, 77);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 2;
+            this.textBox6.Text = "Hotel Name";
+            // 
+            // bookingDataSet
+            // 
+            this.bookingDataSet.DataSetName = "BookingDataSet";
+            this.bookingDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // hotelsBindingSource
+            // 
+            this.hotelsBindingSource.DataMember = "Hotels";
+            this.hotelsBindingSource.DataSource = this.bookingDataSet;
+            // 
+            // hotelsTableAdapter
+            // 
+            this.hotelsTableAdapter.ClearBeforeFill = true;
+            // 
+            // addNumberToHotelToolStripMenuItem
+            // 
+            this.addNumberToHotelToolStripMenuItem.Name = "addNumberToHotelToolStripMenuItem";
+            this.addNumberToHotelToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.addNumberToHotelToolStripMenuItem.Text = "AddNumberToHotel";
+            this.addNumberToHotelToolStripMenuItem.Click += new System.EventHandler(this.addNumberToHotelToolStripMenuItem_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox9);
+            this.groupBox3.Controls.Add(this.textBox10);
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.textBox7);
+            this.groupBox3.Controls.Add(this.textBox8);
+            this.groupBox3.Location = new System.Drawing.Point(12, 37);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(661, 273);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Delete Hotel";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(414, 151);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(154, 62);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Add";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(6, 51);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 3;
+            this.textBox7.Text = "Hotel Id";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(6, 77);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 2;
+            this.textBox8.Text = "RoomNumber";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(6, 103);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 6;
+            this.textBox9.Text = "Price";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(6, 129);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 5;
+            this.textBox10.Text = "num category";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 334);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "BookingAdminPanel";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bookingDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hotelsBindingSource)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem administratorOptionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addNewHotelToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripMenuItem deleteAHotelToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.ToolStripMenuItem hotelsViewToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private BookingDataSet bookingDataSet;
+        private System.Windows.Forms.BindingSource hotelsBindingSource;
+        private BookingDataSetTableAdapters.HotelsTableAdapter hotelsTableAdapter;
+        private System.Windows.Forms.ToolStripMenuItem addNumberToHotelToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+    }
+}
+
---
+++ BookingAdminPanel/BookingAdminPanel/BookingAdminPanel/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace BookingAdminPanel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "bookingDataSet.Hotels". При необходимости она может быть перемещена или удалена.
+            this.hotelsTableAdapter.Fill(this.bookingDataSet.Hotels);
+            groupBox1.Visible = false;
+            groupBox2.Visible = false;
+            groupBox3.Visible = false;
+            dataGridView1.Visible = false;
+        }
+
+        private void addNewHotelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            groupBox3.Visible = false;
+            groupBox1.Visible = true;
+            groupBox2.Visible = false;
+            dataGridView1.Visible = false;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string locationName = textBox1.Text;
+            string name = textBox2.Text;
+            int category = Convert.ToInt16(textBox3.Text);
+            string pictUrl = textBox4.Text;
+            BookingDataSet.HotelsDataTable hotels = new BookingDataSet.HotelsDataTable();
+            BookingDataSetTableAdapters.HotelsTableAdapter ta = new BookingDataSetTableAdapters.HotelsTableAdapter();
+            SqlConnection sda = ta.Connection;
+            SqlCommand comm = new SqlCommand("insert into Hotels(hotelLocation,name,hotelCategory,pictUrl) values('" + locationName + "','" + name + "'," + category.ToString() + ",'" + pictUrl + "'" + ")",sda);
+            comm.Connection.Open();
+            comm.ExecuteNonQuery();
+            comm.Connection.Close();
+        }
+
+        private void deleteAHotelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            groupBox3.Visible = false;
+            groupBox1.Visible = false;
+            groupBox2.Visible = true;
+            dataGridView1.Visible = false;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string locationName = textBox5.Text;
+            string name = textBox6.Text;
+                        
+            BookingDataSet.HotelsDataTable hotels = new BookingDataSet.HotelsDataTable();
+            BookingDataSetTableAdapters.HotelsTableAdapter ta = new BookingDataSetTableAdapters.HotelsTableAdapter();
+            SqlConnection sda = ta.Connection;
+            SqlCommand comm = new SqlCommand("delete from Hotels where hotelLocation = '" + locationName + "' and name = '" + name+"'", sda);
+            comm.Connection.Open();
+            comm.ExecuteNonQuery();
+            comm.Connection.Close();
+        }
+
+        private void hotelsViewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            groupBox3.Visible = false;
+            groupBox1.Visible = false;
+            groupBox2.Visible = true;
+            dataGridView1.Visible = true;
+            
+            BookingDataSet.HotelsDataTable hotels = new BookingDataSet.HotelsDataTable();
+            BookingDataSetTableAdapters.HotelsTableAdapter ta = new BookingDataSetTableAdapters.HotelsTableAdapter();
+            SqlConnection sda = ta.Connection;
+            SqlCommand comm = new SqlCommand("select * from Hotels", sda);
+            comm.Connection.Open();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(comm);
+            da.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+           // dataGridView1.
+            comm.Connection.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void addNumberToHotelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            groupBox1.Visible = false;
+            groupBox2.Visible = false;
+            dataGridView1.Visible = false;
+            groupBox3.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int hotelId = Convert.ToInt16(textBox7.Text);
+            int RoomNumber = Convert.ToInt16(textBox8.Text);
+            int category = Convert.ToInt16(textBox10.Text);
+            float price = (float)Convert.ToDouble(textBox9.Text);
+            BookingDataSet.HotelNumbersDataTable hotelNumbers = new BookingDataSet.HotelNumbersDataTable();
+            BookingDataSetTableAdapters.HotelNumbersTableAdapter ta = new BookingDataSetTableAdapters.HotelNumbersTableAdapter();
+            SqlConnection sda = ta.Connection;
+            SqlCommand comm = new SqlCommand("insert into HotelNumbers(number,price,hId,catNum) values(" + RoomNumber.ToString() + "," + price.ToString() + "," + hotelId.ToString() + "," + category.ToString() + ")", sda);
+            comm.Connection.Open();
+            comm.ExecuteNonQuery();
+            comm.Connection.Close();
+        }
+    }
+}
